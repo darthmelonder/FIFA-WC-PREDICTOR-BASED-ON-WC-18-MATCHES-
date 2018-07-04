21f264db445834c14bab535e4e0a2a7edaceb84f
darthmelonder <darthmelonder@users.noreply.github.com>
2018-07-04 11:45:30 +0530

    Add files via upload

--- predict_semis.py
+++ predict_semis.py
@@ -1,8 +1,20 @@
+
+# coding: utf-8
+
+# In[31]:
+
+
 import pandas as pd
 # FIFA TEAM STATS FOR WC 2018 upto R16
 df = pd.read_csv('fifa_team_stats.csv')
 # MATCHES RESULTS IN WC 2018 upto R16
 matches = pd.read_csv('ALL_MATCHES.csv')
+matches.head()
+
+
+# In[32]:
+
+
 
 # Normailizing
 matches['goals_scored_1']=matches['goals_scored_1']/matches['matches_played_1']
@@ -28,7 +40,36 @@ matches['shots_attempted_1']=matches['shots_attempted_1']/matches['matches_playe
 matches['attempts_off_target_1']=matches['attempts_off_target_1']/matches['matches_played_1']
 
 matches['shots_blocked_1']=matches['shots_blocked_1']/matches['matches_played_1']
+matches.head()
+matches_2 = pd.DataFrame()
+visited=[]
+for col in matches.columns:
+    c_name = str(col)
+    if ('result' not in c_name):
+        col_names = c_name.split('_')
+        y = len(col_names)
+        c_name_2 = ""
+        for j in range(y-1):
+            if (c_name_2):
+                c_name_2 = c_name_2 + "_"
+            c_name_2=c_name_2 + col_names[j]
+        if (c_name_2 not in visited):
+            matches_2[c_name_2+"_1"]=matches[col]
+            visited.append(c_name_2)
+        else:
+            matches_2[c_name_2+"_2"]=matches[col]
+    else:
+        matches_2['results']=1-matches['results']
+matches_2.head()
+matches=matches.append(matches_2)
+matches = matches.reset_index(drop=True)
+matches[matches['Team_1']=='Russia']
 
+
+# In[33]:
+
+
+#Difference in Opposing Teams
 matches['diff_goals_scored']=matches['goals_scored_1']-matches['goals_scored_2']
 
 matches['diff_goals_against']=matches['goals_against_1']-matches['goals_against_2']
@@ -40,10 +81,15 @@ matches['diff_shots_attempted']=matches['shots_attempted_1']-matches['shots_atte
 matches['diff_attempts_off_target']=matches['attempts_off_target_1']-matches['attempts_off_target_2']
 
 matches['diff_shots_blocked']=matches['shots_blocked_1']-matches['shots_blocked_2']
+matches.head()
 
+
+# In[34]:
+
+
+#Training Data
 columns_of_interest = ['diff_goals_scored','diff_goals_against','diff_attempts_on_target','diff_shots_attempted','diff_attempts_off_target','diff_shots_blocked']    
 
-matches_2 = matches 
 train_X = matches[columns_of_interest]
 
 train_Y = matches['results']
@@ -56,6 +102,12 @@ train_X[train_X.isnull()]
 train_X=train_X.dropna()
 
 rf.fit(train_X,train_Y)
+
+
+# In[35]:
+
+
+#Function to return weight of wining of team_1 against team_2
 
 def winning_probability (team_1,team_2):
     test_matches = pd.DataFrame()
@@ -96,10 +148,21 @@ def winning_probability (team_1,team_2):
         pred_Y = rf.predict(test_X)
         return pred_Y[0]
 
-# Quarter Finals Winning Probabilites: #
-print ('Uruguay vs France -> %f' %(winning_probability(['Uruguay'],['France'])))
-print ('Belgium vs Brazil -> %f' %(winning_probability(['Belgium'],['Brazil'])))
-print ('England vs Sweden -> %f' %(winning_probability(['England'],['Sweden'])))
-print ('Croatia vs Russia -> %f' %(winning_probability(['Croatia'],['Russia'])))
+#Function to return probability of winning of Team 1 against Team 2
 
+def get_probability (team_1,team_2):
+    X1 = winning_probability(team_1,team_2)
+    X2 = winning_probability(team_2,team_1)
+    X = X1/(X1+X2)
+    return X
+
+
+# In[40]:
+
+
+# Quarter Finals Winning Probabilites: #
+print ('Uruguay vs France -> %f' %(get_probability(['Uruguay'],['France'])))
+print ('Belgium vs Brazil -> %f' %(get_probability(['Belgium'],['Brazil'])))
+print ('England vs Sweden -> %f' %(get_probability(['England'],['Sweden'])))
+print ('Croatia vs Russia -> %f' %(get_probability(['Croatia'],['Russia'])))
 
